db18027a4ac9cb35676f97f64ac21aa2c173d166
Tomas Carnecky <tomas.carnecky@gmail.com>
2006-08-24 09:26:32 +0000

    Update toc for 1.12, lots of bugfixes

--- Lib/CommChannel.lua
+++ Lib/CommChannel.lua
@@ -34,28 +34,11 @@ end
 	The AddOn
 ]]
 
-local function encode(text)
-	text = string.gsub(text, "([h°])", "°%1")
-	return text.."°"
-end
-
-local function decode(text)
-	text = string.gsub(text, "([Ss])h", "%1")
-	text = string.gsub(text, "°([h°])", "%1")
-	text = string.gsub(text, "^(.*)°.-$", "%1")
-	return text
-end
---[[
-	Manager Frame
-]]
-
-local delayMap = {
-	["PLAYER_ENTERING_WORLD"] = 8,
-	["GUILD_ROSTER_UPDATE"] = 4,
-	["PARTY_MEMBERS_CHANGED"] = 2,
-	["RAID_ROSTER_UPDATE"] = 2,
-	["CHAT_MSG_RAID"] = 2,
-}
+local sfind = string.find
+local sformat = string.format
+local sgsub = string.gsub
+local slen = string.len
+local ssub = string.sub
 
 local argLists = { }
 setmetatable(argLists, { __mode = "kv" })
@@ -76,137 +59,29 @@ local function loadArgs(s)
 end
 
 local function onEvent()
-	if (event == "CHAT_MSG_CHANNEL") then
-		for _, Channel in lib.Channels do
-			if (Channel.Spec.Current and arg8 == GetChannelName(Channel.Spec.Current)) then
-				if (Channel.Spec.Sender.Validate(arg2)) then
-					arg1 = decode(arg1)
-					local _, _, module, func, argString = string.find(arg1, "(%a-):(%a-)%((.*)%)")
-					if (module and func and argString) then
-						local object = Channel.Modules[module]
-						if (object and object[func]) then
-							local argList = loadArgs(argString)
-							
-							if (argList) then
-								argList[1] = object
-								object[func](unpack(argList))
-							end
-						end
-					end
-				end
-			end
-		end
-		
-		return
-	elseif (event == "CHAT_MSG_RAID") then
-		local _, _, token = string.find(arg1, "Securing CommChannel: (.*)")
-		if (token) then
-			lib.Channels["raid"].Spec.Name.Token = (token == "(none)") and nil or token
-		end
-	end
-	
-	lib:UpdateChannels(assert(delayMap[event]))
-end
-
-local function onUpdate()
-	-- TODO: use CHAT_MSG_CHANNEL_NOTICE to find out when we've joined the channels
-	if (arg1 > 1/5) then
-		return
-	end
-	
-	lib.Slave.Delay = lib.Slave.Delay - arg1
-	if (lib.Slave.Delay < 0) then
-		if (lib.Slave.Action == "Leave") then
-			lib.Slave.Action = "Join"
-			lib.Slave.Delay = 1
-			
-			leaveChannels()
-		else
-			lib.Slave.Action = nil
-			lib.Slave:Hide()
-			
-			joinChannels()
-		end
-	end
-end
-
-if (lib.Slave == nil) then
-	lib.Slave = CreateFrame("Frame", "CommChannelSlave")
-
-	lib.Slave:RegisterEvent("PLAYER_ENTERING_WORLD")
-	lib.Slave:RegisterEvent("GUILD_ROSTER_UPDATE")
-	lib.Slave:RegisterEvent("PARTY_MEMBERS_CHANGED")
-	lib.Slave:RegisterEvent("RAID_ROSTER_UPDATE")
-	lib.Slave:RegisterEvent("CHAT_MSG_CHANNEL")
-	lib.Slave:RegisterEvent("CHAT_MSG_RAID")
-
-	lib.Slave:SetScript("OnEvent", onEvent)
-	lib.Slave:SetScript("OnUpdate", onUpdate)
-	
-	lib.Slave.Delay = 0
-end
-
-
---[[
-	Private Functions
-]]
-
-function leaveChannels()
---	local sig = string.format("leaveChannels()")
-
-	for k, Channel in lib.Channels do
-		Channel.Spec.Current = next(Channel.Modules) and Channel.Spec.Name.Generate() or nil
-		
-		local channelList = { GetChannelList() }
-		local channelListCount = table.getn(channelList)
-		
-		for listIndex=1, channelListCount, 2 do
-			local chatChannelName = channelList[listIndex + 1]
-			
-			if (string.find(chatChannelName, Channel.Spec.Name.Regexp)) then
-				if (chatChannelName ~= Channel.Spec.Current) then
---					DEFAULT_CHAT_FRAME:AddMessage(sig..": leaving stale channel: "..chatChannelName)
-					LeaveChannelByName(chatChannelName)
-				elseif (next(Channel.Modules) == nil) then
---					DEFAULT_CHAT_FRAME:AddMessage(sig..": leaving channel with no registered modules "..chatChannelName)
-					LeaveChannelByName(chatChannelName)
+	if (arg1 == "CommChannel") then
+		local Channel = lib.Channels[arg3]
+		local _, _, module, func, argString = sfind(arg2, "^(%a-):(%a-)%((.*)%)$")
+		if (module and func and argString) then
+			local object = Channel[module]
+			if (object and object[func]) then
+				local argList = loadArgs(argString)
+				if (argList) then
+					argList[1] = object
+					object[func](unpack(argList))
 				end
 			end
 		end
 	end
 end
 
-function joinChannels()
---	local sig = string.format("joinChannels()")
-
-	for k, Channel in lib.Channels do
-		if (next(Channel.Modules)) then
-			if (Channel.Spec.Current) then
-				Channel.Spec.Current = string.sub(Channel.Spec.Current, 1, 26)
-				JoinChannelByName(Channel.Spec.Current)
-				
---				DEFAULT_CHAT_FRAME:AddMessage(sig..": joining: "..Channel.Spec.Current)
-			end
-		end
-	end
+if (lib.Channels == nil) then
+	lib.Channels = { ["GUILD"] = { }, ["PARTY"] = { }, ["RAID"] = { }, ["BATTLEGROUND"] = { } }
+	
+	lib.Slave = CreateFrame("Frame")
+	lib.Slave:RegisterEvent("CHAT_MSG_ADDON")
 end
-
-function lib:UpdateChannels(delay)
-	if (lib.Slave.Delay < delay or lib.Slave.Action == "Join") then
-		lib.Slave.Delay = delay
-		lib.Slave.Action = "Leave"
-		lib.Slave:Show()
-	end
-end
-
-function lib:ChannelSpec(channel, spec, old)
---	local sig = string.format("CommChannel:Channel(%s, [spec])", channel)
-
-	self.Channels[channel] = { }
-	self.Channels[channel].Spec = spec
-	self.Channels[channel].Modules = old and old.Modules or { }
-end
-
+lib.Slave:SetScript("OnEvent", onEvent)
 
 
 --[[
@@ -227,11 +102,13 @@ local function serializeObject(luaObject)
 	if (luaObject == nil) then
 		return "" 
 	elseif type(luaObject) == "string" then
-		return string.format("%q", luaObject)
+		return sformat("%q", luaObject)
 	elseif type(luaObject) == "table" then
 		local serializedString = "{"
- 
-		if luaObject[1] and table.getn(luaObject) == getTableCount(luaObject) then
+		
+		if (next(luaObject) == nil) then
+			return "{}"
+		elseif luaObject[1] and table.getn(luaObject) == getTableCount(luaObject) then
 			for i = 1, table.getn(luaObject) do
 				serializedString = serializedString..serializeObject(luaObject[i])..","
 			end
@@ -249,7 +126,7 @@ local function serializeObject(luaObject)
 			end
 		end
 
-		return string.sub(serializedString, 0, string.len(serializedString) - 1).."}"
+		return ssub(serializedString, 0, slen(serializedString) - 1).."}"
 	elseif type(luaObject) == "number" then
 		return tostring(luaObject)
 	elseif type(luaObject) == "boolean" then
@@ -273,14 +150,14 @@ function clientInterface:Call(func, ...)
 end
 
 function lib:Create(channel, module, iface)
-	local sig = string.format("CommChannel:Create(%q, %q, [iface])", channel, module)
+	local sig = sformat("CommChannel:Create(%q, %q, [iface])", channel, module)
 	local Channel = self.Channels[channel]
 	if (Channel == nil) then
 		DEFAULT_CHAT_FRAME:AddMessage(sig..": unknown channel")
 		return
 	end
 	
-	if (Channel.Modules[module]) then
+	if (Channel[module]) then
 		DEFAULT_CHAT_FRAME:AddMessage(sig..": module is already registered")
 		return
 	end
@@ -290,7 +167,7 @@ function lib:Create(channel, module, iface)
 		return
 	end
 	
-	Channel.Modules[module] = iface
+	Channel[module] = iface
 
 	local clientModule = { }
 	setmetatable(clientModule, clientMetatable)
@@ -301,26 +178,24 @@ function lib:Create(channel, module, iface)
 end
 
 function lib:Destroy(channel, module)
-	local sig = string.format("CommChannel:Destroy(%q, %q)", channel, module)
+	local sig = sformat("CommChannel:Destroy(%q, %q)", channel, module)
 	local Channel = self.Channels[channel]
 	if (Channel == nil) then
 		DEFAULT_CHAT_FRAME:AddMessage(sig..": unknown channel")
 		return
 	end
 	
-	if (Channel.Modules[module] == nil) then
+	if (Channel[module] == nil) then
 		DEFAULT_CHAT_FRAME:AddMessage(sig..": module is not registered")
 		return
 	end
 	
-	Channel.Modules[module] = nil
-
-	lib:UpdateChannels(4)
+	Channel[module] = nil
 end
 
 
 function lib:Call(channel, module, func, ...)
-	local sig = string.format("CommChannel:Call(%q, %q, %q, ...)", channel, module, func)
+	local sig = sformat("CommChannel:Call(%q, %q, %q, ...)", channel, module, func)
 	local Channel = self.Channels[channel]
 	if (Channel == nil) then
 		DEFAULT_CHAT_FRAME:AddMessage(sig..": unknown channel")
@@ -331,43 +206,17 @@ function lib:Call(channel, module, func, ...)
 	local success, msg = pcall(serializeObject, arg)
 	
 	if (not success) then
-		msg = string.gsub(msg, "Interface\\AddOns\\(.*)\\CommChannel.lua:(%d+): ", "")
+		msg = sgsub(msg, "(.*)CommChannel.lua:(%d+): ", "")
 		DEFAULT_CHAT_FRAME:AddMessage(sig..": error in serializeObject(): "..msg)
 		return
 	end
 	
-	local msg = string.sub(msg, 2, string.len(msg) - 1)
-	msg = encode(module..":"..func.."("..msg..")")
+	local msg = module..":"..func.."("..ssub(msg, 2, slen(msg) - 1)..")"
 	
-	if (string.len(msg) > 255) then
+	if (slen(msg) > (255 - 12)) then
 		DEFAULT_CHAT_FRAME:AddMessage(sig..": channelMessage too big")
 		return
 	end
 	
-	if (Channel.Spec.Current) then
---		DEFAULT_CHAT_FRAME:AddMessage(sig..": sending message to channel: "..channelMessage)
-		SendChatMessage(msg, "CHANNEL", nil, GetChannelName(Channel.Spec.Current))
-	else
---		DEFAULT_CHAT_FRAME:AddMessage(sig..": no active channel")
-	end
-end
-
-function lib:Secure(channel, token)
-	local sig = string.format("CommChannel:Secure(%q, %q)", channel, token)
-	
-	if (channel == "raid") then
-		for raidID=1,GetNumRaidMembers() do
-			local name, rank = GetRaidRosterInfo(raidID)
-			if (name == UnitName("player")) then
-				if (rank < 2) then
-					DEFAULT_CHAT_FRAME:AddMessage(string.format("%s: Only the raid leader can secure the raid channel.", sig))
-					return
-				end
-			end
-		end
-		
-		SendChatMessage("RAID", "Securing CommChannel: "..(token or "(none)"))
-	else
-		DEFAULT_CHAT_FRAME:AddMessage(string.format("%s: Channel not supported.", sig))
-	end
+	SendAddonMessage("CommChannel", msg, channel)
 end
